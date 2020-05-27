edb938010514ec25693b71749186fe11daa75a91
Swawm <swawm@DESKTOP.localadmin>
2020-05-27 20:24:54 +0300

    typo

--- scenes/smg_projectile.gd
+++ scenes/smg_projectile.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
-const FIREBALL_SPEED = 200
-var motion = Vector2(1, 0) * FIREBALL_SPEED	
 
-onready var player = $"/root/Player"
 
+const BULLET_SPEED = 200
+var motion = Vector2(1, 0) * BULLET_SPEED	
+
+onready var bullet = $Sprite
 
 func _ready():
 	set_process(true)
-	
+	Player.connect("smg_picked_up", self, "_on_smg_picked_up")
+
 	
 func _physics_process(delta):
 	position = position + motion * delta 
@@ -18,3 +20,10 @@ func _physics_process(delta):
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	pass # Replace with function body.
+
+
+func _on_smg_projectile_body_entered(_body):
+	queue_free()
+	pass # Replace with function body.
+	
+
