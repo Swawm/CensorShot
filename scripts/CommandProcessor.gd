f79f2c56b7a34dbaabaf69fcef53e10587034d62
Swawm <swawm@DESKTOP.localadmin>
2021-04-29 16:26:08 +0300

    saves

--- scripts/CommandProcessor.gd
+++ scripts/CommandProcessor.gd
@@ -7,11 +7,11 @@ func process_command(input: String):
 	var first_word = words[0]
 	match first_word:
 		"help":
-			return "Print exit to leave" + '\n' + 'Print clear to clear screen output' + '\n' + 'Print ls to see directory content'
+			return "Print exit to leave" + '\n' + 'Print clear to clear screen output' + '\n' + 'Print ls to see directory content' 
 		"fuck":
 			return "Do not be rude, please"
 		"exit":
-			get_tree().change_scene("res://levels/test2.tscn")
+			SimpleSave.load_scene(get_tree(), "res://save_slots/snapshot0.tscn")
 			return "okay"
 		"how":
 			return "You dont have friends, do you?"
@@ -20,9 +20,10 @@ func process_command(input: String):
 		'docker':
 			return "docker is not installed on this system"
 		'ls':
-			return 'open_door.sh' + "\n" + 'hello.sh'
+			return 'open_door.sh' + "\n" + 'hello.sh' + "\n" + 'show_logs.sh'
 		'./hello.sh':
 			return "Welcome to LFS terminal." + "\n" + "Print help for more info" + "\n" + "Don't forget to leave feedback at" + "\n" + "http://34.73.15.119/"
-			
+		'./show_logs.sh':                                                                                                                                                                                           
+			return "It rains in Hapros. To be honest," + "\n" + "I don't now how we can win against Alandor's" + "\n" + "army of the dead. Seems to me, there is no" + "\n" + "escape in this war, but just wait until" + "\n" + "you will become one of them"
 	if first_word != "help":
 		return "Error"
--- scripts/Computer.gd
+++ scripts/Computer.gd
@@ -4,8 +4,8 @@ func interaction_can_interact(interactionComponentParent : Node) -> bool:
 	return interactionComponentParent is Player
 	
 func interaction_interact(interactionComponentParent : Node) -> void:
+	SimpleSave.save_scene(get_tree(), "res://save_slots/snapshot0.tscn")
 	get_tree().change_scene("res://scenes/Terminal.tscn")
-	pass
 	
 func interaction_get_text() -> String:
 	return "Terminal" 
